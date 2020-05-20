8f494a6ad25c4b4b2c7a2745f4249b35300f2a77
DarkUlixes <reiarifi@gmail.com>
2020-05-20 14:00:48 +0200

    changed render method

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,35 +1,34 @@
-l=document.getElementsByTagName("td").length;
-lr=false;
-for( c = 3; c < l; c++ )
-{
-	bcgr="";
-	if(!lr)
-	{
-		bcgr="#ffffff";
-	}
-	
-	if( (c+1)%3 === 0 )
-	{
-		lr=!lr;
-	}
+const url = 'ws://localhost:8080'
+ws = new WebSocket(url);
+ws.onmessage = (event) => {
+	data = JSON.parse(event.data);
+	updateAll(data);
+	style();
+};
 
-	document.getElementsByTagName("td")[c].style.background = bcgr;
+function updateAll(data) {
+	parentElement = document.getElementsByTagName('tbody')[0];
+	parentElement.innerHTML =`
+		<tr id='head'>
+			<th class="header">Currency</th>
+			<th class="header">Buy</th>
+			<th class="header">Sell</th>
+		</tr>`
+	for(currency of data){
+		parentElement.innerHTML += `
+		<tr class="currency" id="${currency.name}">
+			<td><img src="/images/${currency.name}.png">${currency.name}</td>
+			<td><div>${currency.buy}</div></td>
+			<td><div>${currency.sell}</div></td>
+		</tr>`;
+	}
 }
 
-l=document.getElementsByTagName("input").length;
-lr=false
-for( c = 0; c < l; c++ )
-{
-	bcgr="";
-	if(lr)
-	{
-		bcgr="#ffffff";
+function style() {
+	colored = true;
+	for( row of document.getElementsByClassName("currency") ) {
+	 	bcgr = colored ? null : "#ebe7e7";
+	 	row.style.background = bcgr;
+	 	colored=!colored;
 	}
-	
-	if( (c+1)%2 === 0 )
-	{
-		lr=!lr;
-	}
-
-	document.getElementsByTagName("input")[c].style.background = bcgr;
 }
