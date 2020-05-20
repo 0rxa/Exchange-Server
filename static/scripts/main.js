170ea01c9b1060f2b9c1e73509d59f6a529bc811
DarkUlixes <reiarifi@gmail.com>
2020-05-20 15:50:46 +0200

    standardize html

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -6,6 +6,13 @@ ws.onmessage = (event) => {
 	style();
 };
 
+function update(currency, data) {
+	element = document.getElementById(currency);
+	const { name, buy, sell } = currency;
+	row = convertToRow(name, buy, sell);
+	currency.innerHTML = row;
+}
+
 function updateAll(data) {
 	parentElement = document.getElementsByTagName('tbody')[0];
 	parentElement.innerHTML =`
@@ -14,13 +21,10 @@ function updateAll(data) {
 			<th class="header">Buy</th>
 			<th class="header">Sell</th>
 		</tr>`
-	for(currency of data){
-		parentElement.innerHTML += `
-		<tr class="currency" id="${currency.name}">
-			<td><img src="/images/${currency.name}.png">${currency.name}</td>
-			<td><div>${currency.buy}</div></td>
-			<td><div>${currency.sell}</div></td>
-		</tr>`;
+	for(currency of data) {
+		const { name, buy, sell } = currency;
+		row = convertToRow(name, buy, sell);
+		parentElement.innerHTML += row;
 	}
 }
 
@@ -31,4 +35,13 @@ function style() {
 	 	row.style.background = bcgr;
 	 	colored=!colored;
 	}
+}
+
+function convertToRow(name, buy, sell){
+	return `
+	<tr class="currency" id="${name}">
+		<td><img src="/images/${name}.png">${name}</td>
+		<td><div>${buy}</div></td>
+		<td><div>${sell}</div></td>
+	</tr>`;
 }
--- static/scripts/update.js
+++
@@ -1,7 +0,0 @@
-function updateValue(a)
-{
-	bval = document.getElementsByClassName("buy")[a-1].value;
-	sval = document.getElementsByClassName("sell")[a-1].value;
-
-	window.location.href = "scripts/update.php?w1=" + a + "&w2=" + sval + "&w3=" + bval;
-}
