72f49606f886a243e76cf7fdddc933e8159054d5
DarkUlixes <reiarifi@gmail.com>
2020-05-20 14:00:21 +0200

    server

--- server.js
+++ server.js
@@ -1,5 +1,64 @@
 const express = require('express')
+const http = require('http')
+const ws = require('ws');
+
 const app = express()
+const server = http.createServer(app);
+const wss = new ws.Server({ server });
+const port = 8080;
+
+const data = [
+	{
+		name: "USD",
+		buy: 1.00,
+		sell: 1.01
+	},
+	{
+		name: "EUR",
+		buy: 1.00,
+		sell: 1.01
+	},
+	{
+		name: "AUD",
+		buy: 1.00,
+		sell: 1.01
+	},
+	{
+		name: "CAD",
+		buy: 1.00,
+		sell: 1.01
+	},
+	{
+		name: "CHF",
+		buy: 1.00,
+		sell: 1.01
+	},
+	{
+		name: "DKK",
+		buy: 1.00,
+		sell: 1.01
+	},
+	{
+		name: "GBP",
+		buy: 1.00,
+		sell: 1.01
+	},
+	{
+		name: "JPY",
+		buy: 1.00,
+		sell: 1.01
+	},
+	{
+		name: "NOK",
+		buy: 1.00,
+		sell: 1.01
+	},
+	{
+		name: "SEK",
+		buy: 1.00,
+		sell: 1.01
+	}
+]
 
 const options = {
 	dotfiles: 'ignore',
@@ -14,3 +73,23 @@ const options = {
 }
 
 app.use(express.static('./static/', options));
+app.get('/', (req, res) => {
+	res.redirect('/index.html');
+})
+
+wss.on('connection', (ws) => {
+	ws.send(JSON.stringify(data));
+
+	ws.on('message', (msg) => {
+		ws.send(msg);
+		ws.emit('rcvmsg');
+	})
+
+	ws.on('rcvmsg', () => {
+		console.log('recieved msg event');
+	});
+})
+
+server.listen(8080, () => {
+	console.log(`listening on ${port}`);
+})
