badf78956c03042b570524baa4a4d259b32e60f2
DarkUlixes <reiarifi@gmail.com>
2020-05-20 18:02:22 +0200

    update interface

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -2,11 +2,13 @@ const url = 'ws://localhost:8080'
 ws = new WebSocket(url);
 ws.onmessage = (event) => {
 	data = JSON.parse(event.data);
-	updateAll(data);
+	if(data.currency === "all") updateAll(data.data);
+	else update(data.currency, data.data);
 	style();
 };
 
 function update(currency, data) {
+	console.log(currency, data);
 	element = document.getElementById(currency);
 	const { name, buy, sell } = currency;
 	row = convertToRow(name, buy, sell);
@@ -41,7 +43,29 @@ function convertToRow(name, buy, sell){
 	return `
 	<tr class="currency" id="${name}">
 		<td><img src="/images/${name}.png">${name}</td>
-		<td><div>${buy}</div></td>
-		<td><div>${sell}</div></td>
+		<td><div class="${name} buy" onclick="makeEditable(this)">${buy}</div></td>
+		<td><div class="${name} sell" onclick="makeEditable(this)">${sell}</div></td>
 	</tr>`;
+}
+
+function makeEditable(element) {
+	element.setAttribute("onkeyup", "send(this)");
+	element.setAttribute("contenteditable", "true");
+}
+
+function send(element) {
+	if(event.key !== "Enter") {
+		return
+	}
+
+	element.setAttribute("contenteditable", "false");
+	classes = element.getAttribute("class");
+	currency = classes.split(" ")[0];
+	buy = document.getElementsByClassName(`${currency} buy`)[0].textContent;
+	sell = document.getElementsByClassName(`${currency} sell`)[0].textContent;
+	ws.send(JSON.stringify({
+		"name": currency,
+		"buy": buy,
+		"sell": sell
+	}));
 }
